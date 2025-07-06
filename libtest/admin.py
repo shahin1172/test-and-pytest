77ac76d89e99abc824c19a8d755203affbe99e75
shahin <schmok1172@gmail.com>
2025-07-06 13:40:10 +0330

    s

--- libtest/admin.py
+++ libtest/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from django.contrib import admin
+from .models import Book, Review, BorrowRequest
+
+admin.site.register(Book)
+admin.site.register(Review)
+admin.site.register(BorrowRequest)
 
 # Register your models here.
